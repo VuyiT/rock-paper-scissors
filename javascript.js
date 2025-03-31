c2db346ff0688acda02f2d9484bdbcba419154ea
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-03-31 08:58:15 +0200

    Write function to get computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("rock")
+// COMPUTER CHOICE
+// The function randomly returns "rock", "paper" or "scissors".
+// let getComputerChoice = () => {return "rock", "paper", "scissors" randomly}
+
+function getComputerChoice () {
+    choice = Math.floor(Math.random() * 3);  // This tells the function to choose between three choices randomly.
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
